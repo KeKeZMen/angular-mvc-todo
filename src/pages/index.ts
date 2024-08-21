a197d94cfc58423d222f3dadc83299c1ebe31e14
KeKeZMen <makeskex@yandex.ru>
2024-08-21 08:36:41 +0300

    added(routing): added root routing
    imported root-routing to app module

---
+++ src/pages/index.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tasks-page').then((m) => m.TasksPageModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
