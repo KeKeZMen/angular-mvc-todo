1f87a6f7620ec3be8808fb656de6b81d61ea877c
KeKeZMen <makeskex@yandex.ru>
2024-08-22 20:29:19 +0300

    fix(di): добавлен импорт async pipe

--- src/organisms/tasks-column/tasks-column.module.ts
+++ src/organisms/tasks-column/tasks-column.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { TasksColumnComponent } from './tasks-column.component';
 import { TaskModule } from '@organisms/task/task.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TasksColumnComponent],
-  imports: [CommonModule, TaskModule, FormsModule],
+  imports: [CommonModule, TaskModule, FormsModule, AsyncPipe],
   exports: [TasksColumnComponent],
 })
 export class TasksColumnModule {}
