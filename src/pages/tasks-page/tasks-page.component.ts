63a9e274dee25fd88e7165be915fdf4aae1a17bd
KeKeZMen <makeskex@yandex.ru>
2024-08-28 09:32:59 +0300

    fix(pages): сокращенная запись получения количества элементов

--- src/pages/tasks-page/tasks-page.component.ts
+++ src/pages/tasks-page/tasks-page.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TaskService } from '@services';
-import { map, Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-tasks-page',
   templateUrl: './tasks-page.component.html',
   styleUrl: './tasks-page.component.css',
 })
-export class TasksPageComponent implements OnInit {
-  public tasksCount$ = new Observable<number>();
-
-  constructor(private taskService: TaskService) {}
-
-  ngOnInit(): void {
-    this.tasksCount$ = this.taskService.tasks$.pipe(
-      map((tasks) => tasks.length)
-    );
-  }
+export class TasksPageComponent {
+  public tasksCount$ = inject(TaskService).tasks$.pipe(
+    map((tasks) => tasks.length)
+  );
 }
