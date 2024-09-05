4c34a8b0c225c4826efe6757fe5820bb3306d0c1
KeKeZMen <makeskex@yandex.ru>
2024-09-05 13:31:46 +0300

    fix(lib): реализация создания таска

--- projects/task-lib/src/lib/task-form/task-form.component.ts
+++ projects/task-lib/src/lib/task-form/task-form.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TaskService } from '../task.service';
+import { Subject, takeUntil } from 'rxjs';
 
 export interface ICreateTaskForm {
   text: FormControl<string | null>;
@@ -12,18 +13,30 @@ export interface ICreateTaskForm {
   styleUrls: ['./task-form.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnDestroy {
   public createTaskForm: FormGroup = new FormGroup<ICreateTaskForm>({
     text: new FormControl(null, Validators.required),
   });
+
+  private unsubscribe$ = new Subject<boolean>();
 
   constructor(private taskService: TaskService) {}
 
   public onSubmit($event: KeyboardEvent) {
     if ($event.key == 'Enter')
       if (this.createTaskForm.valid) {
-        this.taskService.createTask(this.createTaskForm.value?.text);
-        this.createTaskForm.reset();
+        this.taskService
+          .createTask(this.createTaskForm.value?.text)
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe(() => {
+            this.createTaskForm.reset();
+            console.log('Задача создана');
+          });
       }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.unsubscribe();
   }
 }
